b3a3839200e151583b558230543f3dd00cd98524
csxeye <39286984+csxeye@users.noreply.github.com>
2020-01-25 17:45:22 -0800

    changes to player_info and queries
    Added new query function and new player_info attributes for information from the snapshots table

--- player_info.py
+++ player_info.py
@@ -9,11 +9,27 @@ class player:
     # Attributes:
     # name: name of the player
     # games_id: a pandas dataframe that contains the games that the player is involved in as well as their in game id
+    # snapshots: a pandas dataframe that contains the snapshots of the current player, i.e. location, angle, HP, armor
     name = attr.ib(default = attr.Factory(str))
     games_id = attr.ib(default = attr.Factory(pd.DataFrame))
-    
+    snapshots = attr.ib(default = attr.Factory(pd.DataFrame))
+
+
     # populate the games_id attribute
     def populate_games_id(self):
         player_query = queries()
         player_query.entities_player_query(self.name)
         self.games_id = player_query.execute_query()
+
+
+    # populate the snapshots attribute with all games that the player is involved in
+    def populate_snapshots(self):
+        snapshots_query = queries()
+
+        for index, row in self.games_id.iterrows(): # this loop goes through all the games the player is involved in
+            snapshots_query.snapshots_game_player_query(row["id"], row["player_id"])
+            cur_game_snapshots = snapshots_query.execute_query()
+            if index == 0:
+                self.snapshots = cur_game_snapshots
+            else:
+                self.snapshots.append(cur_game_snapshots)
--- queries.py
+++ queries.py
@@ -2,6 +2,8 @@ import attr
 import pandas as pd
 import psycopg2
 
+entities_table = "public.entities"
+snapshots_table = "public.snapshots"
 
 connection = psycopg2.connect(user = "postgres",
                                   password = "admin",
@@ -17,6 +19,7 @@ class queries:
     # Attributes:
     # query: the string of the query
     query = attr.ib(default = attr.Factory(str))
+
     
     # build a query for getting player information from public.entities
     def entities_player_query(self, name):
@@ -24,9 +27,22 @@ class queries:
                     SELECT
                         *
                     from
-                        public.entities
+                        """ + entities_table + """
                     where
                         player_name = '""" + name + """'"""
+
+
+    # build a query for getting information from public.snapshots of a particular player in a particular game
+    def snapshots_game_player_query(self, game_id, player_id):
+        self.query = """
+                    SELECT
+                        *
+                    from
+                        """ + snapshots_table + """
+                    where
+                        id = '""" + str(game_id) + """' AND
+                        player_id = '""" + str(player_id) + """'"""
+
     
     # execute query to pandas dataframe
     def execute_query(self):
