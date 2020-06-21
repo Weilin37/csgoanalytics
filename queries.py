c5cbb569cd31f75221af3a2b72beee179ede54fe
csx-ai <60117589+csx-ai@users.noreply.github.com>
2020-06-21 01:20:12 -0400

    Added new query type for ticks, added frst player statstic

--- queries.py
+++ queries.py
@@ -4,10 +4,11 @@ import psycopg2
 
 entities_table = "public.entities"
 snapshots_table = "public.snapshots"
+ticks_table = "public.ticks"
 
 connection = psycopg2.connect(user = "postgres",
-                                  password = "admin",
-                                  host = "localhost",
+                                  password = "goodgame",
+                                  host = "demos.cdeosts5l1qu.us-east-2.rds.amazonaws.com",
                                   port = "5432",
                                   database = "postgres")
 
@@ -42,6 +43,25 @@ class queries:
                     where
                         id = '""" + str(game_id) + """' AND
                         player_id = '""" + str(player_id) + """'"""
+
+    # build a query for getting information from public.ticks of a particular in a partciular game
+    def ticks_game_player_query(self, game_id, player_id):
+        self.query = """
+                    SELECT
+                        *
+                    from
+                        """ + ticks_table + """
+                    where
+                        id = '""" + str(game_id) + """' AND
+                        (fire_entity = '""" + str(player_id) + """' OR
+                         jump_entity = '""" + str(player_id) + """' OR
+                         hurt_entity = '""" + str(player_id) + """' OR
+                         flashed_entity = '""" + str(player_id) + """' OR
+                         footstep_entity = '""" + str(player_id) + """' OR
+                         swap_team_entity = '""" + str(player_id) + """' OR
+                         kill_killer = '""" + str(player_id) + """' OR
+                         kill_victim = '""" + str(player_id) + """' OR
+                         kill_assister = '""" + str(player_id) + """')"""
 
     
     # execute query to pandas dataframe
